85691d0636ef4bf8ef230814c7a8b37c5b916127
Bha108jain <bhavyajainn1008@gmail.com>
2025-07-12 21:11:48 +0530

    image issue fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "randomuser.me",
+      },
+    ],
+  },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
